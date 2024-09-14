f9d8a88ed6bcca7254836e5b65c9f5876f37bab1
OptimusCrime <thomas.gautvedt@gmail.com>
2024-09-14 21:55:09 +0000

    Deploying to gh-pages from @ OptimusCrime/hltv-no-spoilers@20aefb32ab21fb34190092016a4005f3d5bbd176 🚀

--- README.md
+++
@@ -1,4 +0,0 @@
-# hltv-no-spoilers
-
-A simple website for browsing matches from HLTV.org without risking spoilers.
-
--- frontend/src/utils/localStorageKeys.ts
+++
@@ -1,4 +0,0 @@
-export enum LocalStorageKeys {
-  LOCAL_STORAGE_KEY_TEAM_ID = 'hltv-no-spoiler-team-id',
-  LOCAL_STORAGE_KEY_TEAM_NAME = 'hltv-no-spoiler-team-name',
-}
